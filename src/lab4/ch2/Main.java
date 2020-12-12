608c8ff92980a0c31bf2eeaeaacf7651cfc00fc3
razvanabagiu89 <abagiurazvan8989@gmail.com>
2020-12-12 10:49:22 +0200

    ch2

---
+++ src/lab4/ch2/Main.java
@@ -0,0 +1,29 @@
+package lab4.ch2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Path file = Path.of("./src/lab4/ch2/input.txt");
+        String string = Files.readString(file);
+        String finalString = "";
+
+        for(int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+
+            if(Character.isUpperCase(c)) {
+                finalString += c;
+            }
+        }
+
+        String output = finalString.replace('X', ' ');
+        System.out.println(output);
+    }
+}
